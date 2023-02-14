7e50d6c8a385710c14b23dab02e3e184a6aec869
徐志斌 <xuzhibin@lucidata.cn>
2023-02-14 21:29:13 +0800

    前端环境搭建

--- src/api/user.js
+++ src/api/user.js
@@ -1,9 +1,10 @@
 import request from '@/utils/request'
 
-export function login() {
+export function login(data) {
   return request({
     url: '/user/login',
-    method: 'post'
+    method: 'post',
+    data
   })
 }
 
