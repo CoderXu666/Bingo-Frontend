11288b19036fc3ec60953791bc82e715b5ddafe0
徐志斌 <1262254123@qq.com>
2023-05-20 20:30:26 +0800

    Bingo前端项目搭建-徐志斌

--- src/views/first/index.vue
+++ src/views/first/index.vue
@@ -1,6 +1,6 @@
-<template xmlns="http://www.w3.org/1999/html">
+<template>
   <el-container>
-    <!--  上边栏  -->
+    <!--  Head Bar  -->
     <el-header class="header-cls">
       <div>
         <!--    LOGO    -->
@@ -10,7 +10,7 @@
           :fit="fit"
         ></el-image>
 
-        <!--    加入我们    -->
+        <!--  Join Us Page  -->
         <span>
           <el-link type="primary" class="join-us" @click="centerDialogVisible = true">如何成为 Committer</el-link>
         </span>
@@ -64,12 +64,11 @@
           <span @click="dialogFormVisible = true">
             <el-avatar class="avatar-cls" :size="50" :src="avatarUrl"></el-avatar>
           </span>
-          <!--          <el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>-->
           <div>
             <el-dialog :center="true" style="text-align: center;" width="35%" title="欢迎登录"
                        :visible.sync="dialogFormVisible">
               <el-form :model="form" style="height: 200px; margin-top: 35px; margin-left: 180px">
-                <div style="margin-top: 20px; width: ">
+                <div style="margin-top: 20px;">
                   <el-input placeholder="请输入账号" v-model="userId" style="width: 280px"></el-input>
                 </div>
                 <div style="margin-top: 20px;">
@@ -136,23 +135,25 @@
       </div>
     </el-header>
 
-    <!--   主体   -->
-    <div class="main-cls">
+    <!--   Main   -->
+    <div class="main-cls" style="background-image: '@/assets/img/img.png'">
       <el-main>
 
       </el-main>
     </div>
 
-    <!--  Join Us Page  -->
-    <el-dialog
-      title="如何成为全民制作人"
-      :visible.sync="centerDialogVisible"
-      width="50%"
-      center
-    >
-      <div style="height: 500px;">
-        <div style="height: 400px; width: 520px; text-align: center; margin:auto">
-          <div style="background : mistyrose; height: 50px; vertical-align:middle; text-align:center">
+    <!--  Frame  -->
+    <div>
+      <!--   Join US   -->
+      <el-dialog
+        title="加入我们"
+        :visible.sync="centerDialogVisible"
+        width="50%"
+        center
+      >
+        <div style="height: 500px;">
+          <div style="height: 400px; width: 520px; text-align: center; margin:auto">
+            <div style="background : mistyrose; height: 50px; vertical-align:middle; text-align:center">
             <span>
               在牢记鸽鸽作品的同时，能够有自己的独特理解，
               <br/>
@@ -160,122 +161,119 @@
               <br/>
               深刻贯彻落实ikun理念，对小黑子们进行严厉谴责，坚定维护一个坤坤原则。
             </span>
-          </div>
-          <div style="background : mistyrose; margin-top: 20px; height: 300px">
-            <span style="font-weight: bold">请荔枝打分并提供建议</span>
-            <div style="margin-top: 20px; margin-left: 100px">
-              <table>
-                <tr>
-                  <td style="width: 120px">
-                    <span style="font-weight: bold">分 数：</span>
-                  </td>
-                  <td>
-                    <div>
-                      <el-rate
-                        v-model="adviceData.score"
-                        show-text
-                        :texts=starText
-                      >
-                      </el-rate>
-                    </div>
-                  </td>
-                </tr>
-              </table>
             </div>
-            <div style=" margin-top: 35px;">
-              <el-input
-                type="textarea"
-                :rows="6"
-                placeholder="请输入您的建议"
-                v-model="adviceData.advice"
-                style="width: 350px"
-              >
-              </el-input>
-            </div>
-            <div style="text-align: center; margin-top: 10px">
+            <div style="background : mistyrose; margin-top: 20px; height: 300px">
+              <span style="font-weight: bold">请荔枝打分并提供建议</span>
+              <div style="margin-top: 20px; margin-left: 100px">
+                <table>
+                  <tr>
+                    <td style="width: 120px">
+                      <span style="font-weight: bold">分 数：</span>
+                    </td>
+                    <td>
+                      <div>
+                        <el-rate
+                          v-model="adviceData.score"
+                          show-text
+                          :texts=starText
+                        >
+                        </el-rate>
+                      </div>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+              <div style=" margin-top: 35px;">
+                <el-input
+                  type="textarea"
+                  :rows="6"
+                  placeholder="请输入您的建议"
+                  v-model="adviceData.advice"
+                  style="width: 350px"
+                >
+                </el-input>
+              </div>
+              <div style="text-align: center; margin-top: 10px">
               <span slot="footer" class="dialog-footer">
                 <el-button type="primary" @click="submitAdvice(adviceData)">提 交</el-button>
               </span>
+              </div>
+            </div>
+            <div style="background : mistyrose; margin-top: 20px; height: 120px">
+              <table style="height: 120px">
+                <tr>
+                  <th style="width: 120px;">
+                    <img src="../../assets/qr_code/微信图片_20230227111138.png">
+                  </th>
+                  <th style="width: 400px">
+                    <span>  欢迎加入我们，</span>
+                    <br/>
+                    <span> 披荆斩棘，伴坤远航。</span>
+                  </th>
+                </tr>
+              </table>
             </div>
           </div>
-          <div style="background : mistyrose; margin-top: 20px; height: 120px">
-            <table style="height: 120px">
-              <tr>
-                <th style="width: 120px;">
-                  <img src="../../assets/qr_code/微信图片_20230227111138.png">
-                </th>
-                <th style="width: 400px">
-                  <span>  欢迎加入我们，</span>
-                  <br/>
-                  <span> 披荆斩棘，伴坤远航。</span>
-                </th>
-              </tr>
-            </table>
+        </div>
+      </el-dialog>
+
+      <!--  VIP  -->
+      <el-dialog
+        style="font-weight: bold"
+        title="VIKN"
+        :visible.sync="memberVisible"
+        width="50%"
+        center
+      >
+        <div style="height: 380px; margin-left: 30px;margin-right: 30px">
+          <div style=" height: 320px;margin-top: 40px; vertical-align:middle; text-align:center">
+            <span>你想和鸽鸽再近一点吗，你也想瞬间爆炸，当场融化吗？为了鸽鸽冲起来吧!!!</span>
+            <el-row style="margin-top: 30px;">
+              <el-col :span="6" style="margin-left: 20px">
+                <el-card :body-style="{ padding: '0px' }" shadow="hover">
+                  <img src="../../assets/member/微信图片_20230228202519.png" class="member-image">
+                  <div style="padding: 14px;">
+                    <span>鸡日起获得一个月的vikun权限</span>
+                    <div class="member-bottom member-clearfix">
+                      <el-button type="text" class="member-button">月度会员</el-button>
+                    </div>
+                  </div>
+                </el-card>
+              </el-col>
+              <el-col :span="6" style="margin-left: 60px">
+                <el-card :body-style="{ padding: '0px' }" shadow="hover">
+                  <img src="../../assets/member/微信图片_20230228201823.png" class="member-image">
+                  <div style="padding: 14px;">
+                    <span>鸡日起获得一个鸡度的vikun权限</span>
+                    <div class="member-bottom member-clearfix">
+                      <el-button type="text" class="member-button">鸡度会员</el-button>
+                    </div>
+                  </div>
+                </el-card>
+              </el-col>
+              <el-col :span="6" style="margin-left: 60px">
+                <el-card :body-style="{ padding: '0px' }" shadow="hover">
+                  <img src="../../assets/member/微信图片_20230228202525.png" class="member-image">
+                  <div style="padding: 14px;">
+                    <span>鸡日起获得一年的vikun权限</span>
+                    <div class="member-bottom member-clearfix">
+                      <el-button type="text" class="member-button">黏度会员</el-button>
+                    </div>
+                  </div>
+                </el-card>
+              </el-col>
+            </el-row>
+          </div>
+          <div style="height: 130px;margin-top: -30px; vertical-align:middle; text-align:center">
+            <span>开通VIKUN后可获得鸽鸽专属表情包，得到VIKUN专属头像，</span>
+            <br/>
+            <span> 同时会给予定量鸡分，在商城中可兑换商品等等...</span>
+            <br/>
+            <span> 众多好礼，拿到坤软！</span>
           </div>
         </div>
-      </div>
-    </el-dialog>
-    <el-dialog
-      style="font-weight: bold"
-      title="VIKN"
-      :visible.sync="memberVisible"
-      width="50%"
-      center
-    >
-      <div style="height: 380px; margin-left: 30px;margin-right: 30px">
-        <div style=" height: 320px;margin-top: 40px; vertical-align:middle; text-align:center">
-          <span>你想和鸽鸽再近一点吗，你也想瞬间爆炸，当场融化吗？为了鸽鸽冲起来吧!!!</span>
-          <el-row style="margin-top: 30px;">
-            <el-col :span="6" style="margin-left: 20px">
-              <el-card :body-style="{ padding: '0px' }" shadow="hover">
-                <img src="../../assets/member/微信图片_20230228202519.png" class="member-image">
-                <div style="padding: 14px;">
-                  <span>鸡日起获得一个月的vikun权限</span>
-                  <div class="member-bottom member-clearfix">
-                    <!--<time class="member-time">{{ currentDate }}</time>-->
-                    <el-button type="text" class="member-button">月度会员</el-button>
-                  </div>
-                </div>
-              </el-card>
-            </el-col>
-            <el-col :span="6" style="margin-left: 60px">
-              <el-card :body-style="{ padding: '0px' }" shadow="hover">
-                <img src="../../assets/member/微信图片_20230228201823.png" class="member-image">
-                <div style="padding: 14px;">
-                  <span>鸡日起获得一个鸡度的vikun权限</span>
-                  <div class="member-bottom member-clearfix">
-                    <!--<time class="member-time">{{ currentDate }}</time>-->
-                    <el-button type="text" class="member-button">鸡度会员</el-button>
-                  </div>
-                </div>
-              </el-card>
-            </el-col>
-            <el-col :span="6" style="margin-left: 60px">
-              <el-card :body-style="{ padding: '0px' }" shadow="hover">
-                <img src="../../assets/member/微信图片_20230228202525.png" class="member-image">
-                <div style="padding: 14px;">
-                  <span>鸡日起获得一年的vikun权限</span>
-                  <div class="member-bottom member-clearfix">
-                    <!--<time class="member-time">{{ currentDate }}</time>-->
-                    <el-button type="text" class="member-button">黏度会员</el-button>
-                  </div>
-                </div>
-              </el-card>
-            </el-col>
-          </el-row>
-        </div>
-        <div style="height: 130px;margin-top: -30px; vertical-align:middle; text-align:center">
-          <span>开通VIKUN后可获得鸽鸽专属表情包，得到VIKUN专属头像，</span>
-          <br/>
-          <span> 同时会给予定量鸡分，在商城中可兑换商品等等...</span>
-          <br/>
-          <span> 众多好礼，拿到坤软！</span>
-        </div>
-      </div>
-    </el-dialog>
-
-    <!--  BJM  -->
-    <audio controls="controls" hidden src="../../assets/bg_music/鸡你太美.mp3" ref="audio"></audio>
+      </el-dialog>
+    </div>
   </el-container>
 </template>
 
@@ -332,7 +330,7 @@ export default {
 
   methods: {
     /**
-     *保存建议和评分
+     * save advice
      */
     submitAdvice(data) {
       saveApi.saveAdvice(data)
@@ -345,17 +343,13 @@ export default {
      * query user information based on user id
      */
     findUserStatistics(useName) {
-      saveApi.findUserStatistics(useName)
-        .then(res => {
-          this.avatarUrl = res.data.headUrl
-          this.follow = res.data.followCount
-          this.fans = res.data.fanCount
-          this.likes = res.data.likeCount
-          console.log(res)
-        })
-        .catch(error => {
-          console.log(error)
-        })
+      saveApi.findUserStatistics(useName).then(res => {
+        this.avatarUrl = res.data.headUrl
+        this.follow = res.data.followCount
+        this.fans = res.data.fanCount
+        this.likes = res.data.likeCount
+        console.log(res)
+      })
     }
   }
 }
@@ -381,7 +375,10 @@ export default {
 }
 
 .main-cls {
-  background-image: url("https://xuzhibin-bucket.oss-cn-beijing.aliyuncs.com/%E5%9B%BE%E7%89%87%E8%B5%84%E6%BA%90/index.gif");
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
   background-size: 100% 100%;
 }
 
