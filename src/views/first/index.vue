0b7b1f9dbe4ab0463baec25602ee582a2dcbad36
徐志斌 <1262254123@qq.com>
2023-05-26 19:15:00 +0800

    Bingo Frontend Code Commit-徐志斌

--- src/views/first/index.vue
+++ src/views/first/index.vue
@@ -7,15 +7,14 @@
                 :fit="fit"></el-image>
       <!--   list   -->
       <el-menu
-        style="width:30%;float: left;margin-top: 6px"
+        style="width:29.5%;float: left;margin-top: 6px;border-bottom: none;"
         mode="horizontal"
         background-color="transparent"
-        text-color="#fff"
-        active-text-color="grey">
-        <el-menu-item index="1" style="margin-left: 7%;color: darkgrey">社区广场</el-menu-item>
-        <el-menu-item index="2" style="margin-left: 2%;color: darkgrey">狂欢派对</el-menu-item>
-        <el-menu-item index="3" style="margin-left: 2%;color: darkgrey">视频直播</el-menu-item>
-        <el-menu-item index="4" style="margin-left: 2%;color: darkgrey">好物商城</el-menu-item>
+      >
+        <el-menu-item index="1" style="margin-left: 6%;color: lightskyblue">社区广场</el-menu-item>
+        <el-menu-item index="2" style="margin-left: 2%;color: lightskyblue">狂欢派对</el-menu-item>
+        <el-menu-item index="3" style="margin-left: 2%;color: lightskyblue">好物商城</el-menu-item>
+        <el-menu-item index="4" style="margin-left: 2%;color: lightskyblue">关于作者</el-menu-item>
       </el-menu>
 
       <!--   avatar   -->
@@ -29,27 +28,27 @@
         </el-badge>
       </div>
       <div style="float: right;margin-right: 2%;margin-top: 1.5%">
+        <el-button size="mini">好友</el-button>
+      </div>
+      <div style="float: right;margin-right: 2%;margin-top: 1.5%">
         <el-button size="mini">会员</el-button>
       </div>
       <div style="float: right;margin-right: 2%;margin-top: 1.5%">
         <el-button size="mini">作品</el-button>
       </div>
-      <div style="float: right;margin-right: 2%;margin-top: 1.5%">
-        <el-button size="mini">发布</el-button>
-      </div>
     </el-header>
 
     <!--  2.Search  -->
     <div class="search-cls">
-      <!--   video   -->
-      <video src="../../../src/assets/video/background.mp4" autoplay muted loop></video>
-      <div style="margin:11% auto;width: 50%">
+      <!--   background video   -->
+      <video src='@/assets/video/background.mp4' autoplay muted loop></video>
+      <div style="margin:11.6% auto;width: 50%">
         <!--    title    -->
         <h1 style="color: whitesmoke;font-family: 'Arial Rounded MT Bold';margin: auto;padding-left: 9%">
           Bingo：致力打造有趣的年轻人社交平台
         </h1>
         <!--    input    -->
-        <div style="margin-top: 54px;opacity: 0.8;">
+        <div style="margin-top: 55px;opacity: 0.8;">
           <el-input id="search-input" v-model=searchContent placeholder="搜索你想要知道的一切......">
             <el-select slot="prepend" v-model=searchType :placeholder=searchType style="width: 80px;">
               <el-option label="帖子" value="1"></el-option>
@@ -64,17 +63,34 @@
 
     <!--  3.Post & Topic  -->
     <el-main class="main-cls">
-      <el-row>
-        <el-col :span="18" class="left-content">
-          1111111111111111111
-        </el-col>
-        <el-col :span="6" class="right-content">
-          11111111111111111111
-        </el-col>
-      </el-row>
+      <div style="margin: auto;width: 84%; border: 1px solid red;">
+        <el-row>
+          <el-col :span="18" class="left-content" style="border: 1px solid red">
+            <span style="background-color: lightskyblue;height: 23px;display: inline-block;"></span>
+            <span style="margin-right: 1%; color: lightskyblue">广场动态</span>
+            <span style="margin-right: 1%; color: lightskyblue">换一批</span>
+            <span style="margin-right: 1%; color: lightskyblue">查看更多</span>
+          </el-col>
+          <el-col :span="6" class="right-content" style="border: 1px solid red; color: lightskyblue">
+            本周热榜
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :span="18" class="left-content" style="border: 1px solid red">
+            <span style="margin-right: 1%">1111</span>
+            <span style="margin-right: 1%">1111</span>
+            <span style="margin-right: 1%">1111</span>
+            <span style="margin-right: 2%">1111</span>
+          </el-col>
+          <el-col :span="6" class="right-content" style="border: 1px solid red">
+            11111111111111111111
+          </el-col>
+        </el-row>
+      </div>
     </el-main>
 
-    <!--  4.Body  -->
+    <!--  4.Party &   -->
     <el-main class="main-cls" style="background-color: #304156">
       <el-row>
         <el-col :span="18" class="left-content">
@@ -107,7 +123,7 @@
 export default {
   data() {
     return {
-      avatarUrl: '',
+      avatarUrl: require('@/assets/avatar/avatar.jpg'),
       fit: 'fill',
       searchType: '帖子',
       searchContent: '',
