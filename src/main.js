2866d9f512d433d459d24a0c1c8281fbe13475af
徐志斌 <1262254123@qq.com>
2023-08-20 13:05:49 +0800

    Code Commit-徐志斌

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import Vue from 'vue'
 import 'normalize.css/normalize.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en'
-import '@/styles/index.scss'
 import './assets/iconfont/iconfont.css'
 import './assets/iconfont/iconfont'
 import App from './App'
@@ -13,7 +11,7 @@ import router from './router'
 if (process.env.NODE_ENV === 'production') {
 }
 
-Vue.use(ElementUI, {locale})
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
