532e8595cba5cc92c5c78be6df9d839da34cc971
徐志斌 <1262254123@qq.com>
2023-09-03 14:13:10 +0800

    Code Commit By 徐志斌

--- src/views/im/chat.vue
+++ src/views/im/chat.vue
@@ -1,118 +1,128 @@
 <template>
-  <div
-    style="background-color: #292A2D;width: 80%;margin: auto;margin-top: 4.5%;border-radius: 20px">
-    <el-row :gutter="16">
-      <!--  左侧列 -->
-      <el-col :span="1" style="margin-top: 26px;height: 580px;">
-        <el-avatar style="margin-left: 20px" shape="circle" :size="55" :src="this.loginUserInfo.avatarUrl"/>
-      </el-col>
+  <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
+    <div
+      style="background-color: #292A2D;width: 80%;margin: auto;border-radius: 20px">
+      <el-row :gutter="14">
+        <!--  左侧列 -->
+        <el-col :span="1" style="margin-top: 26px;height: 600px;">
+          <el-avatar style="margin-left: 20px" shape="circle" :size="55" :src="this.loginUserInfo.avatarUrl"/>
+        </el-col>
 
-      <!--  好友列表 -->
-      <el-col :span="5" style="margin-top: 26px;margin-left: 3%">
-        <div class="friend-item"
-             v-for="item in userList"
-             style="background-color: #323335;margin-top: 5px;margin-bottom: 5px;border-radius: 10px"
-             @mouseover="hoverEffect($event, true)"
-             @mouseout="hoverEffect($event, false)"
-             @click="clickFriend(item.userId, item.avatarUrl, item.nickName)"
-        >
-          <el-avatar style="float: left;" shape="square" :size="55" :src="item.avatarUrl"/>
-          <div style="float: left;margin-left: 2%;margin-top:3.5%;font-size: 15px">
-            {{ item.nickName }}
-          </div>
-          <span style="float: right;margin-top: 3.5%;margin-right: 3.5%;color: #999999;font-size: 13px">
+        <!--  好友列表 -->
+        <el-col :span="5" style="margin-top: 26px;margin-left: 3%">
+          <div class="friend-item"
+               v-for="item in userList"
+               style="background-color: #323335;margin-top: 5px;margin-bottom: 5px;border-radius: 10px"
+               @mouseover="hoverEffect($event, true)"
+               @mouseout="hoverEffect($event, false)"
+               @click="clickFriend(item.userId, item.avatarUrl, item.nickName)"
+          >
+            <el-avatar style="float: left;" shape="square" :size="55" :src="item.avatarUrl"/>
+            <div style="float: left;margin-left: 2%;margin-top:3.5%;font-size: 15px">
+              {{ item.nickName }}
+            </div>
+            <span style="float: right;margin-top: 3.5%;margin-right: 3.5%;color: #999999;font-size: 13px">
             11:02
           </span>
-          <div style="padding-top: 35px;margin-left: 19%;font-size: 13px;color: #999999">
-            {{ item.nickName }}：1111
+            <div style="padding-top: 35px;margin-left: 19%;font-size: 13px;color: #999999">
+              {{ item.nickName }}：1111
+            </div>
           </div>
-        </div>
-      </el-col>
+        </el-col>
 
-      <!--  聊天框  -->
-      <el-col :span="13" style="background-color: #323335;border-radius: 10px;margin-top: 30px">
-        <div v-if="curChatInfo.userId != ''">
-          <!--  上边栏  -->
-          <div style="height: 60px; display: flex; align-items: center;padding-left: 1%;">
-            <el-avatar :size="46" :src="curChatInfo.avatarUrl" shape="square"
-                       style="margin-right: 1%;cursor:pointer;"/>
-            <div style="font-size: 17px;color: antiquewhite">{{ curChatInfo.userName }}</div>
-          </div>
+        <!--  聊天框  -->
+        <el-col :span="13" style="background-color: #323335;border-radius: 10px;margin-top: 30px">
+          <div v-if="curChatInfo.userId != ''">
+            <!--  上边栏  -->
+            <div style="height: 60px; display: flex; align-items: center;padding-left: 1%;">
+              <el-avatar :size="46" :src="curChatInfo.avatarUrl" shape="square"
+                         style="margin-right: 1%;cursor:pointer;"/>
+              <div style="font-size: 17px;color: antiquewhite">{{ curChatInfo.userName }}</div>
+            </div>
 
-          <!--  聊天内容展示  -->
-          <div id="chat-content-show" style="height: 440px;border-radius: 10px;">
-            <div v-for="item in chatContentList[curChatInfo.userId]">
-              <!--  接收信息  -->
-              <div v-if="item.userId !== loginUserInfo.userId"
-                   style="width: 90%;display: flex;align-items: center;margin-left: 1%;margin-top: 1%;">
+            <!--  聊天内容展示  -->
+            <div id="chat-content-show" style="height: 460px;border-radius: 10px;">
+              <div v-for="item in chatContentList[curChatInfo.userId]">
+                <!--  接收信息  -->
+                <div v-if="item.userId !== loginUserInfo.userId"
+                     style="width: 90%;display: flex;align-items: center;margin-left: 1%;margin-top: 1%;">
                 <span style="margin-right: 6px">
                   <el-avatar :src=item.avatarUrl shape="square" style="cursor:pointer"/>
                 </span>
-                <div style="background-color: #34A8FF;border-radius: 10px;">
-                  <div style="padding: 15px;font-size: 14px;word-break: break-all;">
-                    {{ item.content }}
+                  <div style="background-color: #34A8FF;border-radius: 10px;">
+                    <div style="padding: 15px;font-size: 14px;word-break: break-all;">
+                      {{ item.content }}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <!--  主动发送  -->
-              <div v-if="item.userId === loginUserInfo.userId"
-                   style="float: right;width: 80%;margin-right: 1%;margin-top:1%;display: flex; align-items: center;justify-content: flex-end">
-                <div
-                  style="margin-left: 1.6%;background-color: mediumspringgreen;border-radius: 10px;display: inline-block;">
-                  <div style="padding: 15px;font-size: 14px;word-break: break-all">
-                    {{ item.content }}
+                <!--  主动发送  -->
+                <div v-if="item.userId === loginUserInfo.userId"
+                     style="float: right;width: 80%;margin-right: 1%;margin-top:1%;display: flex; align-items: center;justify-content: flex-end">
+                  <div
+                    style="margin-left: 1.6%;background-color: mediumspringgreen;border-radius: 10px;display: inline-block;">
+                    <div style="padding: 15px;font-size: 14px;word-break: break-all">
+                      {{ item.content }}
+                    </div>
                   </div>
-                </div>
-                <span style="margin-left: 6px">
+                  <span style="margin-left: 6px">
                   <el-avatar :src=item.avatarUrl shape="square" style="cursor:pointer;"/>
                 </span>
+                </div>
               </div>
             </div>
+            <!--  内容输入Container  -->
+            <div id="chat-input-container">
+              <!-- 语音消息 -->
+              <svg style="width: 2em;height: 2em;margin-right: 1%;cursor:pointer;">
+                <use xlink:href="#icon-biaoqianA01_wode-55"></use>
+              </svg>
+              <!-- 输入框 -->
+              <span style="width: 70%">
+                <el-input id="chat-input-id" placeholder="请开始你的表演......" v-model="chatMsg"></el-input>
+              </span>
+              <!-- emoji -->
+              <svg style="width: 2em;height: 2em;margin-left: 1%;cursor:pointer;">
+                <use xlink:href="#icon-biaoqianA01_wode-55"></use>
+              </svg>
+              <!-- 语音、视频 -->
+              <svg style="width: 2em;height: 2em;;cursor:pointer;">
+                <use xlink:href="#icon-biaoqianA01_wode-55"></use>
+              </svg>
+              <!-- 礼物打赏 -->
+              <svg style="width: 2em;height: 2em;;cursor:pointer;">
+                <use xlink:href="#icon-biaoqianA01_wode-55"></use>
+              </svg>
+              <!-- 文件 -->
+              <svg style="width: 2em;height: 2em;;cursor:pointer;">
+                <use xlink:href="#icon-biaoqianA01_wode-55"></use>
+              </svg>
+              <!-- 发送 -->
+              <svg style="width: 2em;height: 2em;;cursor:pointer;">
+                <use xlink:href="#icon-biaoqianA01_wode-55"></use>
+              </svg>
+            </div>
           </div>
-          <!--  内容输入Container  -->
-          <div id="chat-input-container">
-            <svg style="width: 2em;height: 2em;margin-right: 1%;cursor:pointer;">
-              <use xlink:href="#icon-biaoqianA01_wode-55"></use>
-            </svg>
-            <span style="width: 76%">
-              <!--  语音输入icon  -->
-              <el-input id="chat-input-id" placeholder="请开始你的表演......" v-model="chatMsg"></el-input>
-            </span>
-            <!--  其他功能icon  -->
-            <svg style="width: 2em;height: 2em;margin-left: 1%;cursor:pointer;">
-              <use xlink:href="#icon-biaoqianA01_wode-55"></use>
-            </svg>
-            <svg style="width: 2em;height: 2em;;cursor:pointer;">
-              <use xlink:href="#icon-biaoqianA01_wode-55"></use>
-            </svg>
-            <svg style="width: 2em;height: 2em;;cursor:pointer;">
-              <use xlink:href="#icon-biaoqianA01_wode-55"></use>
-            </svg>
-            <svg style="width: 2em;height: 2em;;cursor:pointer;">
-              <use xlink:href="#icon-biaoqianA01_wode-55"></use>
-            </svg>
-          </div>
-        </div>
-      </el-col>
+        </el-col>
 
-      <!--  用户信息、功能框架 -->
-      <el-col :span="4" style="margin-top: 30px" v-if="curChatInfo.userId != ''">
-        <!--  用户信息、群聊列表  -->
-        <div style="height: 340px;background-color: #323335;border-radius: 10px">
-          用户信息
-        </div>
-        <!--  Emoji  -->
-        <Picker
-          style="width: 200px;height: 190px;background-color: #323335;margin-top: 16px;"
-          :include="['people']"
-          :showSearch="false"
-          :showPreview="false"
-          :showCategories="false"
-          @select="addEmoji"
-          :i18n="i18n"
-        />
-      </el-col>
-    </el-row>
+        <!--  用户信息、功能框架 -->
+        <el-col :span="4" style="margin-top: 30px" v-if="curChatInfo.userId != ''">
+          <!--  用户信息、群聊列表  -->
+          <div style="height: 340px;background-color: #323335;border-radius: 10px">
+            用户信息
+          </div>
+          <!--  Emoji  -->
+          <Picker
+            style="width: 200px;height: 216px;background-color: #323335;margin-top: 10px;"
+            :include="['people']"
+            :showSearch="false"
+            :showPreview="false"
+            :showCategories="false"
+            @select="addEmoji"
+            :i18n="i18n"
+          />
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
